33f59bbb793308a2c4d8a0adb54b4f4bef8b9dd7
tarak <ktarkeshwar305@gmail.com>
2020-09-04 19:25:52 +0530

    Sample Splash And Home Fragment Added

---
+++ app/src/main/java/com/example/eduthon_aimers/SplashFragment.kt
@@ -0,0 +1,35 @@
+package com.example.eduthon_aimers
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =inflater.inflate(R.layout.fragment_splash, container, false)
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()    // in Fragment
+        Handler().postDelayed( {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        },2500)
+        return view
+    }
+    override fun onResume() {
+        super.onResume()
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+    override fun onPause() {
+        super.onPause()
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+    }
+}
