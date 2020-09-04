11aea31a3dea7da74c4e633afc86ac57c5793b85
tarak <ktarkeshwar305@gmail.com>
2020-09-04 21:50:21 +0530

    FAQ Fragment Edited

--- app/src/main/java/com/example/eduthon_aimers/HomeFragment.kt
+++ app/src/main/java/com/example/eduthon_aimers/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
@@ -12,7 +15,12 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view =  inflater.inflate(R.layout.fragment_home, container, false)
+        view.buttonFAQ.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_faqsFragment)
+        }
+
+        return view
     }
 
 }
