10f3c28486b9d0bae7481e87f7e038d66df8ba89
coder2699 <pranshu0414@gmail.com>
2020-09-05 03:47:17 +0530

    Created Sol/Complaint Layout

---
+++ app/src/main/java/com/example/eduthon_aimers/Cyber/SolutionFragment.kt
@@ -0,0 +1,20 @@
+package com.example.eduthon_aimers.Cyber
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.eduthon_aimers.R
+
+class SolutionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view= inflater.inflate(R.layout.fragment_solution, container, false)
+        return view
+    }
+}
