9d9e1769eed9c66358eb8054039926fe36508d64
coder2699 <pranshu0414@gmail.com>
2020-09-05 23:46:55 +0530

    Splash Screen Added

--- app/src/main/java/com/example/eduthon_aimers/SplashFragment.kt
+++ app/src/main/java/com/example/eduthon_aimers/SplashFragment.kt
@@ -3,12 +3,13 @@ package com.example.eduthon_aimers
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.os.Handler
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_splash.view.*
 
 class SplashFragment : Fragment() {
 
@@ -19,8 +20,10 @@ class SplashFragment : Fragment() {
         // Inflate the layout for this fragment
         val view =inflater.inflate(R.layout.fragment_splash, container, false)
         (activity as AppCompatActivity?)!!.supportActionBar!!.hide()    // in Fragment
+        view.content.startRippleAnimation()
         Handler().postDelayed( {
             findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            view.content.stopRippleAnimation()
         },2500)
         return view
     }
