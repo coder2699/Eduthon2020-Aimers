6abf418972dcfcc896369052abfe3e400e618742
tarak <ktarkeshwar305@gmail.com>
2020-09-05 13:57:20 +0530

    water and Yoga Fragment Modified

--- app/src/main/java/com/example/eduthon_aimers/Health/WaterFragment.kt
+++ app/src/main/java/com/example/eduthon_aimers/Health/WaterFragment.kt
@@ -1,12 +1,30 @@
 package com.example.eduthon_aimers.Health
 
+import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.eduthon_aimers.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 class WaterFragment : Fragment() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.menuForm -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.don -> {
+
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -14,5 +32,12 @@ class WaterFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_water, container, false)
     }
-
+    override fun onResume() {
+        super.onResume()
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+    override fun onPause() {
+        super.onPause()
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+    }
 }
