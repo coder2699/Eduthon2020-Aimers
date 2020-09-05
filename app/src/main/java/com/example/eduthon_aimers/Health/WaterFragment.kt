7bb25e2445f67b3c5a922f011645df8107a630c9
coder2699 <pranshu0414@gmail.com>
2020-09-06 03:55:33 +0530

    Checkpoint3 HomeFragment Completed

--- app/src/main/java/com/example/eduthon_aimers/Health/WaterFragment.kt
+++ app/src/main/java/com/example/eduthon_aimers/Health/WaterFragment.kt
@@ -35,7 +35,7 @@ class WaterFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_water, container, false)
-        view.navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        view?.navigationView?.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         return view
 
     }
--- app/src/main/java/com/example/eduthon_aimers/HomeFragment.kt
+++ app/src/main/java/com/example/eduthon_aimers/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.example.eduthon_aimers
 
+import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment : Fragment() {
@@ -16,27 +19,57 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view =  inflater.inflate(R.layout.fragment_home, container, false)
-        view.buttonFAQ.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_faqsFragment)
-        }
-        view.buttonHealth.setOnClickListener {
+        view.healthFragmentLaunch.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_healthFragment)
+
+
         }
-        view.buttonRef.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_referenceFragment)
-        }
-        view.buttonArticle.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_articleFragment)
-        }
-        view.buttonCyber.setOnClickListener {
+        view.imageView6.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_cyberFragment)
         }
-
-        view.buttonVid.setOnClickListener {
+        view.imageView5.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_videoActivity)
         }
+        view?.AimageView?.setOnClickListener {
 
+            findNavController().navigate(R.id.action_homeFragment_to_articleFragment)
+
+        }
+        view.yoga1.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_yogaFragment)
+        }
+        view.resFragmentLaunch.setOnClickListener {
+
+            findNavController().navigate(R.id.action_homeFragment_to_referenceFragment)
+        }
+
+        view.faqFragmentLaunch.setOnClickListener {
+
+            findNavController().navigate(R.id.action_homeFragment_to_faqsFragment)
+
+        }
+        //val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+        view.navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         return view
     }
-
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.menuForm -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.don -> {
+                findNavController().navigate(R.id.action_homeFragment_to_donateFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+    override fun onResume() {
+        super.onResume()
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+    override fun onPause() {
+        super.onPause()
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+    }
 }
