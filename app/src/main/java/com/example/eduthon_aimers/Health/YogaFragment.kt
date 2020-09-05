c7458fae059a75034483a6b24e19459d5d17b178
tarak <ktarkeshwar305@gmail.com>
2020-09-05 09:06:07 +0530

    Yoga and Water Layout added

---
+++ app/src/main/java/com/example/eduthon_aimers/Health/YogaFragment.kt
@@ -0,0 +1,18 @@
+package com.example.eduthon_aimers.Health
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.eduthon_aimers.R
+class YogaFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_yoga, container, false)
+    }
+
+}
